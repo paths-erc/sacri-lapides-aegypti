8b6f017bf87e154f965e01bef18551b9bd6c9c71
ddomizia <domizia.derasmo@gmail.com>
2021-10-26 12:05:46 +0200

    Pagina 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,101 @@
+//React
 import * as React from "react";
-import { Link } from "gatsby";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
+//Gatsby
+import { withPrefix } from "gatsby";
+import styled from "styled-components";
 
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
+//Bootstrap
+import { Container, Row, Col } from "react-bootstrap";
+
+//Components
+import Layout from "../templates/IndexLayout";
+import HeaderImage from "../components/HeaderImage";
 
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout>
+      <HeaderImage></HeaderImage>
+      <Wrapper>
+        <section className="section bottom-slant-gray">
+          <Container>
+            <Row>
+              <Col md={{ span: 6, offset: 3 }}>
+                <h1>404</h1>
+                <h2>Sorry, this page does not exist</h2>
+                <a href={withPrefix(`/`)}>
+                  <h3>Go to Home</h3>
+                </a>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+      </Wrapper>
+    </Layout>
   );
 };
+
+//styles
+const Wrapper = styled.section`
+  a {
+    font-family: "Montserrat", sans-serif;
+    color: rgb(130, 36, 51, 0.5) !important;
+    text-decoration: none;
+    text-align: center !important;
+  }
+  a:hover {
+    font-family: "Montserrat", sans-serif;
+    color: rgb(130, 36, 51) !important;
+    text-decoration: underline;
+    text-align: center !important;
+  }
+  h1 {
+    font-family: "Montserrat", sans-serif;
+    text-align: center;
+    color: rgb(130, 36, 51);
+    font-size: 8rem;
+  }
+  h2 {
+    font-family: "Montserrat", sans-serif;
+    text-align: center;
+    color: #777777;
+  }
+  h3 {
+    font-family: "Montserrat", sans-serif;
+    font-size: 3rem;
+  }
+  section {
+    padding: 0;
+    position: relative;
+    z-index: 2;
+  }
+  .bottom-slant-gray {
+    position: relative;
+    padding-top: 1.5%;
+    padding-bottom: 1%;
+    background-color: #f8f9fa;
+  }
+  .bottom-slant-gray:after {
+    content: "";
+    width: 100%;
+    height: 260px;
+    background: #fff;
+    z-index: -1;
+    bottom: -200px;
+    background-color: #f8f9fa;
+    left: 0;
+    position: absolute;
+    -webkit-transform: skewY(-6deg);
+    -ms-transform: skewY(-6deg);
+    transform: skewY(-6deg);
+    -webkit-transform-origin: top left;
+    -ms-transform-origin: top left;
+    transform-origin: top left;
+  }
+  .container {
+    margin-bottom: 10rem;
+  }
+`;
 
 export default NotFoundPage;
