6656f2b3e863ae696eb6b919982a7ce24f3994c5
ddomizia <domizia.derasmo@gmail.com>
2021-11-16 21:50:29 +0100

    Eliminati spazi in "Sacri lapides Aegypti"

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -30,15 +30,14 @@ const About = () => {
             <Col>
               <Title title="About" align="left" />
               <p className="about">
-                The “<em>Sacri lapides Aegypti</em> ” project aims to study and
+                The “<em>Sacri lapides Aegypti</em>” project aims to study and
                 document the processes of transformation of sacred spaces for
                 worship, identity and memory from Pharaonic and Ptolemaic-Roman
                 Egypt to Christian Egypt through topographical-functional
                 analysis and the creation of a general catalogue, in digital
                 form, of the temple structures transformed into Christian places
                 used for the production and preservation of books, as well as
-                their cultic function.<br></br> “<em>Sacri lapides Aegypti</em>{" "}
-                ” is therefore the conceptual premise and the archaeological
+                their cultic function.<br></br> “<em>Sacri lapides Aegypti</em>” is therefore the conceptual premise and the archaeological
                 deepening of the ERC project “PAThs”, whose aim is to define a
                 geography of Coptic manuscript production through the creation
                 of a digital atlas, searchable thematically and chronologically.
